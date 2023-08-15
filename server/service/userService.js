18b74f0b941a3aa22364fd055ffaa8ac4033455a
Ajay <user@Ankitas-MacBook-Air.local>
2023-08-15 16:06:53 +0530

    changes in userService addUser

--- server/service/userService.js
+++ server/service/userService.js
@@ -14,12 +14,14 @@ const getUser = async (column = "_id", value = "") => {
 // add user
 const addUser = async (params) => {
   const hashed = await Common.hashPassword(params.password);
-  console.log(hashed, "pass");
 
   const userData = new UserModel({
     name: params.name,
     email: params.email,
     password: hashed,
+    userName: params.email.split("@")[0],
+    profile: "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg",
+    about: "Hello I am " + ' ' + params.name,
   });
 
   const savedUser = await userData.save();
