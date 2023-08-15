fc091c5926e6a80f55f785e7b00c4a6b8d8504c2
aaqibgouher <aaqibgouher@gmail.com>
2023-08-15 12:38:25 +0530

    model process ongoing

---
+++ server/models/ContactModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// contact token schema
+const contactSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserModel",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// model
+const ContactModel = mongoose.model("ContactModel", contactSchema, "contacts");
+
+// exporting model
+module.exports = ContactModel;
